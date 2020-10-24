02dc1487f7e44d4ee72b253952a1070aabe46159
Igor Shabarin <igorshabarin2000@mail.ru>
2020-10-24 13:20:49 +0300

    feat: add task 4

--- src/Controller.php
+++ src/Controller.php
@@ -3,58 +3,178 @@
 namespace Igshab2000\minesweeper\Controller;
 
 use function Igshab2000\minesweeper\View\showGame;
-use function Igshab2000\minesweeper\Model\create;
-use function Igshab2000\minesweeper\Model\createCells;
-use function Igshab2000\minesweeper\Model\isBomb;
+use function Igshab2000\minesweeper\View\InformationOfGames;
+use function Igshab2000\minesweeper\View\showTurnInfo;
+
+use function Igshab2000\minesweeper\Model\makeVars;
+use function Igshab2000\minesweeper\Model\createMakeArr;
+use function Igshab2000\minesweeper\Model\itsBombs;
 use function Igshab2000\minesweeper\Model\openArea;
 use function Igshab2000\minesweeper\Model\setFlag;
+use function Igshab2000\minesweeper\Model\insertInfo;
+use function Igshab2000\minesweeper\Model\postGameId;
+use function Igshab2000\minesweeper\Model\insertTurnInfo;
+use function Igshab2000\minesweeper\Model\getVars;
+
+function isCorrect($x, $y) {
+    if (is_numeric($x) && is_numeric($y)) {
+        $temp = METERING - 1;
+        if ($x >= 0 && $x <= $temp && $y >= 0 && $y <= $temp) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateDatabase($gameResult) {
+    $gameDatabase = new \SQLite3('gamedb.db');
+    $gameId = GAME_ID;
+
+    $query = "UPDATE gamesInfo
+        SET gameResult = '$gameResult'
+        WHERE idGame = '$gameId'";
+    $gameDatabase->exec($query);
+}
 
 function gameLoop() {
-    global $cells, $lostGame, $openedCellsCount;
-
+    global $cellsArray, $openedCellsCount;
+    $flag = "-";
     $turnCount = 1;
-
     while (true) {
         showGame($turnCount);
-        $turnCount++;
-        
+
         $inputString = \cli\prompt(
-            "Введите координаты x, y через запятую "
-            . "без пробела.\n"
-            . "Для установки флага введите A после координат"
+            "Введите координаты x, y ячейки через "
+            . "запятую без пробела, для установки "
+            . "флага в ячейку введите "
+            . "F или f после ввода координат (через запятую, "
+            . "без пробела)"
         );
 
         $inputArray = explode(',', $inputString);
-        if (
-            !isset($inputArray[0]) || !isset($inputArray[1])
-            || preg_match('/^[0-9]{1}$/', $inputArray[0]) == 0
-            || preg_match('/^[0-9]{1}$/', $inputArray[1]) == 0
-        ) {
+
+        $coordX = $inputArray[0];
+        $coordY = $inputArray[1];
+
+        if (!isCorrect($coordX, $coordY)) {
             \cli\line("Неверно введены данные! Попробуйте еще раз");
-            $turnCount--;
+            continue;
+        }
+
+        if (isset($inputArray[2])) {
+            $flag = $inputArray[2];
+            setFlag($coordX, $coordY);
+            insertTurnInfo($turnCount, "Установлен флаг", $coordX, $coordY);
+            $turnCount++;
+            continue;
+        }
+
+        if (itsBombs($coordX, $coordY)) {
+            showGame($turnCount);
+            \cli\line("GAME OVER");
+            insertTurnInfo($turnCount, "Игра проиграна", $coordX, $coordY);
+            updateDatabase("Игра проиграна");
+            break;
         } else {
-            if (isset($inputArray[2]) && ($inputArray[2] == 'A' || $inputArray[2] == 'a')) {
-                setFlag($inputArray[0], $inputArray[1]);
-            } else {
-                if (isBomb($inputArray[0], $inputArray[1])) {
-                    showGame($turnCount);
-                    \cli\line("GAME OVER");
-                    break;
-                } else {
-                    openArea($inputArray[0], $inputArray[1]);
-                    if ($openedCellsCount == count($cells) * count($cells[0])) {
-                        showGame($turnCount);
-                        \cli\line("CONGRATULATIONS! YOU WON");
-                        break;
-                    }
-                }
+            openArea($coordX, $coordY);
+            insertTurnInfo($turnCount, "Открыта область", $coordX, $coordY);
+            if ($openedCellsCount == count($cellsArray) * count($cellsArray[0])) {
+                showGame($turnCount);
+                \cli\line("CONGRATULATIONS! YOU WON");
+                insertTurnInfo($turnCount, "Игра выиграна", $coordX, $coordY);
+                updateDatabase("Игра выиграна");
+                break;
             }
+        }
+        $turnCount++;
+    }
+}
+
+function newGame() {
+    makeVars();
+    insertInfo();
+    postGameId();
+    createMakeArr("new", GAME_ID);
+    gameLoop();
+    exit();
+}
+
+function listGames() {
+    if (!file_exists("gamedb.db")) {
+        \cli\line("База данных не обнаружена!");
+        return;
+    }
+    $gameDatabase = new \SQLite3('gamedb.db');
+    $result = $gameDatabase->query("SELECT * FROM gamesInfo");
+
+    while ($row = $result->fetchArray()) {
+        InformationOfGames($row);
+        $query = "SELECT 
+            gameTurn, 
+            coordinates, 
+            result 
+            FROM concreteGame 
+            WHERE idGame='$row[0]'
+            ";
+        $gameTurns = $gameDatabase->query($query);
+        while ($gameTurnsRow = $gameTurns->fetchArray()) {
+            showTurnInfo($gameTurnsRow);
         }
     }
 }
 
+function idExists($id) {
+    $gameDatabase = new \SQLite3('gamedb.db');
+    $query = "SELECT EXISTS(SELECT 1 FROM gamesInfo WHERE idGame='$id')";
+    $flag = $gameDatabase->querySingle($query);
+    if ($flag == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function replayGame($id) {
+    if (!file_exists("gamedb.db")) {
+        \cli\line("База данных не создана!");
+        return;
+    }
+
+    if (!idExists($id)) {
+        \cli\line("Выбранной игры не существует");
+        return;
+    } else {
+        getVars($id);
+        insertInfo();
+        postGameId();
+        createMakeArr("replay", $id);
+        gameLoop();
+        exit();
+    }
+}
+
 function startGame() {
-    create();
-    createCells();
-    gameLoop();
+    while (true) {
+        $command = \cli\prompt(
+            "Введите один из доступных ключей:\n"
+            . "--new - новая игра\n"
+            . "--list - вывод списка всех игр\n"
+            . "--replay id - повтор игры с идентивикатором id\n"
+            . "--exit - выход\n"
+        );
+        if ($command == "--new") {
+            newGame();
+        } elseif ($command == "--list") {
+            listGames();
+        } elseif (preg_match('/(^--replay [0-9]+$)/', $command) != 0) {
+            $temp = explode(' ', $command);
+            $id = $temp[1];
+            unset($temp);
+            replayGame($id);
+        } elseif ($command == "--exit") {
+            exit();
+        } else {
+            \cli\line("Неверный ключ! Выберите один из предложенных!");
+        }
+    }
 }
--- src/View.php
+++ src/View.php
@@ -2,44 +2,33 @@
 
 namespace Igshab2000\minesweeper\View;
 
-function showGame($turnCount) {
-    global $cells;
-    \cli\line(sprintf('%20s', "ХОД №" . $turnCount));
-    $line = sprintf('%2s', ' ');
+use function Igshab2000\minesweeper\Model\seeSymbol;
 
-    for ($i = 0; $i < MAX_X; $i++) {
+function InformationOfGames($row) {
+    \cli\line(
+        "ID: $row[0]\nДата: $row[1] $row[2]\nИмя игрока: $row[3]\nРазмерность: "
+        . "$row[4]\nКолличесвово бомб: $row[5]\nСтатус игры: $row[6]"
+    );
+}
+
+function showTurnInfo($row) {
+    \cli\line("Номер хода: $row[0]; координаты: $row[1]; статус игры: $row[2]");
+}
+
+function showGame($turnCount) {
+    \cli\line(sprintf('%18s', "ХОД №" . $turnCount));
+    $line = sprintf('%2s', ' ');
+    for ($i = 0; $i < METERING; $i++) {
         $line .= sprintf('%2s', $i);
     }
-    
     \cli\line($line);
     $line = '';
 
-    for ($i = 0; $i < MAX_Y; $i++) {
+    for ($i = 0; $i < METERING; $i++) {
         $line .= sprintf('%2s', $i);
-
-        for ($j = 0; $j < MAX_X; $j++) {
-            if ($cells[$i][$j]['opened'] == true) {
-                if ($cells[$i][$j]['marked'] == true) {
-                    $line .= sprintf('%2s', 'F');
-                } else {
-                    if ($cells[$i][$j]['isBomb'] == true) {
-                        $line .= sprintf('%2s', '*');
-                    } else {
-                        if ($cells[$i][$j]['besidecount'] == 0) {
-                            $line .= sprintf('%2s', '-');
-                        } else {
-                            $line .= sprintf(
-                                '%2s',
-                                $cells[$i][$j]['besidecount']
-                            );
-                        }
-                    }
-                }
-            } else {
-                $line .= sprintf('%2s', '.');
-            }
+        for ($j = 0; $j < METERING; $j++) {
+            $line .= seeSymbol($i, $j);
         }
-
         \cli\line($line);
         $line = '';
     }
